3ffb1d2c1b7ac86d0ab850b049e0d6d386aff700
Matthew Cowen <svlnsr3f@students.codeinstitute.net>
2024-11-13 10:01:44 +0000

    port fix

--- my_project/settings.py
+++ my_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-j02n-frz$yb_gu47=gbqzupa&2f3+7*5fn9u8=_i0&#w5p6)gi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3000-mattcowen-ithinkthere4-35e69bph3cs.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-mattcowen-ithinkthere4-35e69bph3cs.ws.codeinstitute-ide.net']
 
 
 # Application definition
